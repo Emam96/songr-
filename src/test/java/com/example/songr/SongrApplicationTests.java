2b4ffa0cc57212fd116180fbe3d69c8e9eca2278
Emam96 <emamsh990@gmail.com>
2021-10-24 21:52:04 +0300

    added tests

--- src/test/java/com/example/songr/SongrApplicationTests.java
+++ src/test/java/com/example/songr/SongrApplicationTests.java
@@ -1,13 +1,59 @@
 package com.example.songr;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
+
+
 class SongrApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testTitle() {
+
+
+		Album album1 = new Album("Blackbird", "Alter bridge","13", "59:17", "https://upload.wikimedia.org/wikipedia/en/1/13/Alterbridge_blackbird.jpg" );
+        String title = album1.getTitle();
+          assertEquals("Blackbird", title);
+
 	}
 
+	@Test
+	void testArtist() {
+
+
+		Album album1 = new Album("Blackbird", "Alter bridge","13", "59:17", "https://upload.wikimedia.org/wikipedia/en/1/13/Alterbridge_blackbird.jpg" );
+		String artist = album1.getArtist();
+		assertEquals("Alter bridge", artist);
+
+	}
+
+	@Test
+	void testNumber() {
+
+
+		Album album1 = new Album("Blackbird", "Alter bridge","13", "59:17", "https://upload.wikimedia.org/wikipedia/en/1/13/Alterbridge_blackbird.jpg" );
+		String count = album1.getSongCount();
+		assertEquals("13", count);
+
+	}
+
+	@Test
+	void testLength() {
+
+
+		Album album1 = new Album("Blackbird", "Alter bridge","13", "59:17", "https://upload.wikimedia.org/wikipedia/en/1/13/Alterbridge_blackbird.jpg" );
+		String count = album1.getLength();
+		assertEquals("59:17", count);
+
+	}
+
+	@Test
+	void testUrl() {
+
+
+		Album album1 = new Album("Blackbird", "Alter bridge","13", "59:17", "https://upload.wikimedia.org/wikipedia/en/1/13/Alterbridge_blackbird.jpg" );
+		String url = album1.getImageUrl();
+		assertEquals("https://upload.wikimedia.org/wikipedia/en/1/13/Alterbridge_blackbird.jpg", url);
+
+	}
 }
